5dd0334bf7c4a40fa9d82c478fd4c7172fff4c7d
oriefy <shahzaibakash@gmail.com>
2019-04-01 18:00:50 +0500

    add all models to admin

--- employee/admin.py
+++ employee/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from employee.models import Employee
+from employee.models import Employee, EducationRecord, RecordImage
 
 
 # Register your models here.
 admin.site.register(Employee)
+admin.site.register(EducationRecord)
+admin.site.register(RecordImage)
